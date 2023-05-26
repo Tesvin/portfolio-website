de8bea91d662643eeb19da1be9cdc55212c7c7b0
stephen azosike <stephenazosike@gmail.com>
2023-05-26 14:57:18 +0100

    portfolio website

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -9,6 +9,7 @@ import FireBase from '../assets/firebase.png';
 import GitHub from '../assets/github.png';
 import Tailwind from '../assets/tailwind.png';
 import Mongo from '../assets/mongo.png';
+import Express from '../assets/express-logo.png'
 
 const Skills = () => {
   return (
@@ -52,6 +53,10 @@ const Skills = () => {
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                     <img className='w-20 mx-auto' src={Mongo} alt='Mongo icon'/>
                     <p className='my-4'>MONGO DB</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={Express} alt='Mongo icon'/>
+                    <p className='my-4'>EXPRESSJS</p>
                 </div>
             </div>
 
